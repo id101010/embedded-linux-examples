dd3082282ff8584366d247693ed8b396b2516950
id101010 <aaron@duckpond.ch>
2016-01-18 02:01:48 +0100

    implemented led handlers

--- examlib/examlib.c
+++ examlib/examlib.c
@@ -50,7 +50,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-
 #include<stdlib.h>
 #include<stdio.h>
 #include<stdint.h>
@@ -72,10 +71,10 @@
 #include<sys/time.h>
 
 /* String to access the ADC4 via sysfs */
-#define AIN4_DEV	"/sys/bus/iio/devices/iio\:device0/in_voltage4_raw"
+#define AIN4_DEV	    "/sys/bus/iio/devices/iio:device0/in_voltage4_raw"
 
 /* Delay value for one second in us */
-#define ONE_SECOND	1000000
+#define ONE_SECOND		1000000
 
 /* Define some useful constants */
 #define BUFFER_SIZE		16
@@ -85,8 +84,8 @@
 #undef DEBUG
 
 /* Delay value in micro seconds */
-#define ONE_MILLISECOND		1000
-#define ONE_SECOND			1000000
+#define ONE_MILLISECOND	1000
+#define ONE_SECOND		1000000
 
 /* Define some useful constants */
 #define SYSFS_PATH		"/sys/class/gpio/"
@@ -97,6 +96,8 @@
 #define OUTPUT			0
 #define INPUT			1
 #define PRESSED			'0'
+#define LED_ON          0
+#define LED_OFF         1
 
 /* Define the GPIO numbers of the BBB-BFH-Cape LEDs */
 #define LED_1			61
@@ -112,15 +113,14 @@
 #define MAX_GPIO		(4)
 
 /* Define index of LEDs and Buttons */
-#define L1			0
-#define L2			1
-#define L3			2
-#define L4			3
-
-#define T1			0
-#define T2			1
-#define T3			2
-#define T4			3
+#define L1	    0
+#define L2	    1
+#define L3		2
+#define L4		3
+#define T1		0
+#define T2		1
+#define T3		2
+#define T4		3
 
 /* Define some useful sysfs constants */
 enum {
@@ -135,55 +135,120 @@ enum {
 /* Static variables */
 static int32_t gpio_led[MAX_GPIO] = {LED_1, LED_2, LED_3, LED_4};
 static int32_t gpio_btn[MAX_GPIO] = {BUTN_1, BUTN_2, BUTN_3, BUTN_4};
-
 static char ON[]  = "0";
 static char OFF[] = "1";
 static char OUT[] = "out";
 static char IN[]  = "in";
 
 /* Other variables */
-int   adc_fd;
-int   charRead;
-float aValue;
-char  adcBuffer[BUFFER_SIZE];
-time_t timerid_1, timerid_2, timerid_3, timerid_4;
-struct sigevent se_timer1, se_timer2, se_timer3, se_timer4;
-struct itimerspec ts_1, ts_2, ts_3, ts_4;
-volatile static int32_t counter1, counter2, counter3, counter4 = 0;
+char  	adcBuffer[BUFFER_SIZE];
+char   	button[3];
+char   	led[3];
+time_t 	timerid_1, timerid_2, timerid_3, timerid_4, timerid_adc, timerid_btn;
+struct 	sigevent se_timer1, se_timer2, se_timer3, se_timer4, se_timer_adc, se_timer_btn;
+struct 	itimerspec ts_1, ts_2, ts_3, ts_4, ts_adc, ts_btn;
+volatile int32_t counter1, counter2, counter3, counter4 = 0;
 
 /* Prototypes */
 void callback_1(union sigval arg);
 void callback_2(union sigval arg);
 void callback_3(union sigval arg);
 void callback_4(union sigval arg);
+void callback_adc(union sigval arg);
+void callback_btn(union sigval arg);
 void init_timer(void (*callback)(union sigval arg), struct sigevent *se, struct itimerspec *ts, time_t *timerid, int nanoseconds, int seconds);
 void start_timer(struct itimerspec *ts, time_t *timerid);
 void signal_ctrlc_handler(int signum);
 void signal_terminate_handler(int signum);
 int sysfs_gpio_handler(uint8_t function, uint32_t gpio, char *val);
+float read_adc_value(void); 
 
 /* Timer1 callback */
 void callback_1(union sigval arg)
 {
-    printf("Counter_1 value: %d\n", counter1++);
+    sysfs_gpio_handler(GPIO_SYSFS_GET_VALUE, gpio_led[L1], &led[L1]);   // Read value
+
+    //printf("L1: %d \n", led[L1]);
+
+    if(led[L1] == '0'){                                                 // Toggle
+        sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[L1], OFF);
+    }
+    if(led[L1] == '1'){
+		sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[L1], ON);
+    }
 }
 
 /* Timer2 callback */
 void callback_2(union sigval arg)
 {
-    printf("Counter_2 value: %d\n", counter2++);
+    sysfs_gpio_handler(GPIO_SYSFS_GET_VALUE, gpio_led[L2], &led[L2]);   // Read value
+
+    //printf("L1: %d \n", led[L1]);
+
+    if(led[L2] == '0'){                                                 // Toggle
+        sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[L2], OFF);
+    }
+    if(led[L2] == '1'){
+		sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[L2], ON);
+    }
 }
 
 /* Timer1 callback */
 void callback_3(union sigval arg)
 {
-    printf("Counter_3 value: %d\n", counter3++);
+    sysfs_gpio_handler(GPIO_SYSFS_GET_VALUE, gpio_led[L3], &led[L3]);   // Read value
+
+    //printf("L1: %d \n", led[L1]);
+
+    if(led[L3] == '0'){                                                 // Toggle
+        sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[L3], OFF);
+    }
+    if(led[L3] == '1'){
+		sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[L3], ON);
+    }
 }
 
 /* Timer2 callback */
 void callback_4(union sigval arg)
 {
-    printf("Counter_4 value: %d\n", counter4++);
+    sysfs_gpio_handler(GPIO_SYSFS_GET_VALUE, gpio_led[L4], &led[L4]);   // Read value
+
+    //printf("L1: %d \n", led[L1]);
+
+    if(led[L4] == '0'){                                                 // Toggle
+        sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[L4], OFF);
+    }
+    if(led[L4] == '1'){
+		sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[L4], ON);
+    }
+}
+
+/* ADC_Timer callback */
+void callback_adc(union sigval arg)
+{
+	float aValue = read_adc_value();
+	sprintf(adcBuffer, "%f", aValue);
+	printf("AIN4: %sV\n", adcBuffer);	// Write output to console
+}
+
+/* Button polling timer callback */
+void callback_btn(union sigval arg)
+{
+    int i; 
+
+	// Read button values
+	for(i = 0; i < 4; i++){
+		sysfs_gpio_handler(GPIO_SYSFS_GET_VALUE, gpio_btn[i], &button[i]);
+	}
+
+	// Set led values
+	for(i = 0; i < 4; i++){
+		if(button[i] == PRESSED){
+			sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[i], ON);
+		} else {
+			sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[i], OFF);
+		}
+	} 
 }
 
 /* Timer initialization */
@@ -222,7 +287,6 @@ void start_timer(struct itimerspec *ts, time_t *timerid)
         perror("timer_settime:");
     }
 }
-
 
 /* sysfs_gpio_handler - handle GPIO operations */
 int sysfs_gpio_handler(uint8_t function, uint32_t gpio, char *val)
@@ -320,13 +384,51 @@ int sysfs_gpio_handler(uint8_t function, uint32_t gpio, char *val)
     return 0;
 }
 
+/* init gpio ports */
+void init_gpio(void)
+{
+    int i; 
+
+    /* Setup gpio sysfs for the LEDs [L1..L4] and Buttons [T1..T4] */
+    for (i=0; i<MAX_GPIO; i++) {
+        sysfs_gpio_handler(GPIO_SYSFS_EXPORT, gpio_led[i], NULL);
+        sysfs_gpio_handler(GPIO_SYSFS_SET_DIRECTION, gpio_led[i], OUT);
+        sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[i], OFF);
+        sysfs_gpio_handler(GPIO_SYSFS_EXPORT, gpio_btn[i], NULL);
+        sysfs_gpio_handler(GPIO_SYSFS_SET_DIRECTION, gpio_btn[i], IN);
+    }
+}
+
+float read_adc_value(void) 
+{
+	int charRead;
+    int32_t adc_fd = open(AIN4_DEV, O_RDONLY);
+    char adc_buffer[BUFFER_SIZE];
+
+    if(adc_fd  < 0){
+      perror("Dev not readable");
+      return adc_fd;
+    }
+
+    charRead = read(adc_fd, adc_buffer, sizeof(adc_buffer));
+    close(adc_fd);
+    
+	if (charRead != -1){
+      adc_buffer[charRead] = '\0';
+      float aValue = (float)atoi(adc_buffer);
+      aValue = (V_REF * aValue) / ((1<<12)-1);
+
+      return aValue;
+    }
+
+    return -1;
+}
 
 /* Handle sigint via ctrl c*/
 void signal_ctrlc_handler(int signum)
 {
 	int i;
     printf("\nExit via Ctrl-C\n\n"); 	// Inform user
-    close(adc_fd);						// close adc file
 
     /* Unexport all selected gpios */
     for (i=0; i<MAX_GPIO; i++) {
@@ -342,7 +444,6 @@ void signal_terminate_handler(int signum)
 {
 	int i;
     printf("\nExit via Ctrl-C\n\n"); 	// Inform user
-    close(adc_fd);						// close adc file
 
     /* Unexport all selected gpios */
     for (i=0; i<MAX_GPIO; i++) {
@@ -356,73 +457,35 @@ void signal_terminate_handler(int signum)
 
 int main(int argc, char *argv[])
 {    
-	sigset_t set;
-    char    button[3];
-    uint8_t i;
+	sigset_t 	set;
 
+	/* SIGTERM handling */
     signal(SIGINT, signal_ctrlc_handler); 				// register signal handler
     signal(SIGTERM, signal_terminate_handler);			// Register SIGTERM signal handler
     sigemptyset(&set);									// Initializes the signalmask to empty
     sigaddset(&set, SIGALRM );							// Set the signal mask for the signal handler
 
-    adc_fd = open(AIN4_DEV, O_RDONLY); 					// open adc file descriptor
-
-    if (adc_fd == -1) {
-        perror("Error: cannot open adc device!\n"); 	// Check for errors
-        return -1;
-    }
-
 	/* Init timers */
-    init_timer(callback_1, &se_timer1, &ts_1, &timerid_1, 0, 1);
+    init_timer(callback_adc, &se_timer_adc, &ts_adc, &timerid_adc, 0, 1);			// adc polling timer
+    init_timer(callback_btn, &se_timer_btn, &ts_btn, &timerid_btn, 500000000, 0);	// button polling timer
+    init_timer(callback_1, &se_timer1, &ts_1, &timerid_1, 0, 1);		
     init_timer(callback_2, &se_timer2, &ts_2, &timerid_2, 500000000, 0);
     init_timer(callback_3, &se_timer3, &ts_3, &timerid_3, 250000000, 0);
     init_timer(callback_4, &se_timer4, &ts_4, &timerid_4, 125000000, 0);
     
     /* Start Timers */
+    start_timer(&ts_adc, &timerid_adc);
+    //start_timer(&ts_btn, &timerid_btn);
     start_timer(&ts_1, &timerid_1);
     start_timer(&ts_2, &timerid_2);
     start_timer(&ts_3, &timerid_3);
     start_timer(&ts_4, &timerid_4);
 
-    /* Setup gpio sysfs for the LEDs [L1..L4] and Buttons [T1..T4] */
-    for (i=0; i<MAX_GPIO; i++) {
-        sysfs_gpio_handler(GPIO_SYSFS_EXPORT, gpio_led[i], NULL);
-        sysfs_gpio_handler(GPIO_SYSFS_SET_DIRECTION, gpio_led[i], OUT);
-        sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[i], OFF);
-        sysfs_gpio_handler(GPIO_SYSFS_EXPORT, gpio_btn[i], NULL);
-        sysfs_gpio_handler(GPIO_SYSFS_SET_DIRECTION, gpio_btn[i], IN);
-    }
+	init_gpio(); // init buttons and leds
+
 
     while (1) {
-
-        charRead = read(adc_fd, adcBuffer, sizeof(adcBuffer));	// get ADC as a string
-        
-		if (charRead != -1) {
-
-            adcBuffer[charRead] = '\0';			// Stringtermination
-
-            aValue = (float) atoi(adcBuffer);	// Voltage calculation
-            aValue = (V_REF * aValue) / (N-1);
-            sprintf(adcBuffer, "%f", aValue);
-
-            printf("AIN4: %sV\n", adcBuffer);	// Write output to console
-            lseek(adc_fd, 0, 0);
-
-        	// Read button values
-			for(i = 0; i < 4; i++){
-				sysfs_gpio_handler(GPIO_SYSFS_GET_VALUE, gpio_btn[i], &button[i]);
-			}
-
-			// Set led values
-			for(i = 0; i < 4; i++){
-				if(button[i] == PRESSED){
-				    sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[i], ON);
-				} else {
-				    sysfs_gpio_handler(GPIO_SYSFS_SET_VALUE, gpio_led[i], OFF);
-				}
-			} 
-        }
-        sleep(1);
+		;
     }
 
     return 0;
